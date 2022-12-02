48357a124ceb740ad1e4d7c5241375fefe179f4b
Ali <102534126+AL1isHere@users.noreply.github.com>
2022-12-02 19:21:53 +0700

    Delete minus directory

--- minus/JavaScript/extra code/methodChaining.js
+++
@@ -1,9 +0,0 @@
-/*method chaining = memanggil lebih dari 1 method 
-                    dalam 1 line code
-*/
-
-let userName = "Hello";
-
-let letter = userName.charAt(0).toUpperCase();
-
-console.log(letter)
